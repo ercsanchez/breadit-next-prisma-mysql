d50e74e42e119b5b188a8c7ccd4a10e90ebd1084
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-18 01:20:18 +0800

    feat: add google signin logic; add google logo

--- src/components/UserAuthForm.tsx
+++ src/components/UserAuthForm.tsx
@@ -1,16 +1,40 @@
-import { FC } from 'react';
+'use client';
+
+import { FC, useState } from 'react';
+import { signIn } from 'next-auth/react';
 
 import { cn } from '@/lib/utils';
 import { Button } from '@/components/ui/Button';
+import { Icons } from '@/components/Icons';
 
 interface UserAuthFormProps extends React.HTMLAttributes<HTMLDivElement> {
   className?: string;
 }
 
 const UserAuthForm: FC<UserAuthFormProps> = ({ className, ...props }) => {
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+
+  const loginWithGoogle = async () => {
+    setIsLoading(true);
+
+    try {
+      await signIn('google');
+    } catch (err) {
+      // toast notification error
+    } finally {
+      setIsLoading(false);
+    }
+  };
+
   return (
     <div className={cn('flex justify-center', className)} {...props}>
-      <Button size="sm" className="w-full">
+      <Button
+        size="sm"
+        className="w-full"
+        onClick={loginWithGoogle}
+        isLoading={isLoading}
+      >
+        {isLoading ? null : <Icons.google className="h-4 w-4 mr-2" />}
         Google
       </Button>
     </div>
