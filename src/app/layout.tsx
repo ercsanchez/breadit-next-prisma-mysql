aaccab661ef3876846af63ffe1bcea7abbe7c88a
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-17 17:56:14 +0800

    feat: add navbar

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import '@/styles/globals.css';
 import { Inter } from 'next/font/google';
 
 import { cn } from '@/lib/utils';
+import Navbar from '@/components/Navbar';
 
 export const metadata = {
   title: 'Breadit',
@@ -24,6 +25,7 @@ export default function RootLayout({
       )}
     >
       <body className="min-h-screen pt-12 bg-slate-50 antialiased">
+        <Navbar />
         <div className="container max-w-7xl mx-auto h-full pt-12">
           {children}
         </div>
