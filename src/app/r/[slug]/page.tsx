9955a84646c9eb30850ae212faf7f0af92974c38
ercsanchez <44195323+ercsanchez@users.noreply.github.com>
2023-06-20 00:35:34 +0800

    feat: add subreddit detail page (/r/:slug) and 404 page

---
+++ src/app/r/[slug]/page.tsx
@@ -0,0 +1,48 @@
+import { notFound } from 'next/navigation';
+
+import { getAuthSession } from '@/lib/auth';
+import { db } from '@/lib/db';
+import { INFINITE_SOCIAL_PAGINATION_RESULTS } from '@/config';
+
+interface PageProps {
+  params: {
+    slug: string;
+  };
+}
+
+const page = async ({ params }: PageProps) => {
+  const session = await getAuthSession();
+  const { slug } = params;
+
+  const subreddit = await db.subreddit.findFirst({
+    where: {
+      name: slug,
+    },
+    include: {
+      posts: {
+        include: {
+          author: true,
+          votes: true,
+          comments: true,
+          subreddit: true,
+        },
+        orderBy: {
+          createdAt: 'desc',
+        },
+        take: INFINITE_SOCIAL_PAGINATION_RESULTS,
+      },
+    },
+  });
+
+  if (!subreddit) return notFound();
+
+  return (
+    <>
+      <h1 className="font-bold text-3xl md:text-4xl h-14">
+        r/{subreddit.name}
+      </h1>
+    </>
+  );
+};
+
+export default page;
